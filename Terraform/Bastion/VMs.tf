c6953a4d017d574ddbb39e3c9abc082de41615cd
Git-UserDS <91082410+Git-UserDS@users.noreply.github.com>
2022-10-05 13:45:01 +0300

    Update VMs.tf

--- Terraform/Bastion/VMs.tf
+++ Terraform/Bastion/VMs.tf
@@ -2,17 +2,6 @@ variable "location_name" {
   default = "westus3"
 }
 
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.0.2"
-#     }
-#   }
-
-#   required_version = ">= 1.1.0"
-# }
-
 provider "azurerm" {
   features {}
 }
